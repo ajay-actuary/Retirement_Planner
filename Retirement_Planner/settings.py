ae615f9cd058585ae9b61a8d40436737ce0975e5
ajay-actuary <70125529+ajay-actuary@users.noreply.github.com>
2020-08-29 14:21:37 +0530

    Update settings.py

--- Retirement_Planner/settings.py
+++ Retirement_Planner/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4@ck%$k$q!j$r*_jd&&j*19yfzrq!aw0tk8qj&@h3i))hsn+$q'
+SECRET_KEY = 'some-secret-key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
